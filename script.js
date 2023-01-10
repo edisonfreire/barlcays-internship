97cde4c516aa0dd5cb85a89daaef11a131f8f43a
edisonfreire <edisonfreire14@gmail.com>
2023-01-10 07:28:04 -0500

    update 1/10

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 $(document).ready( function () {
     $('#table_id').DataTable();
+    $('#example tfoot th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
 } );
 
-$('#table_id').DataTable( {
+$('#example').DataTable( {
     paging: true,
     "ajax": "data.json",
     "columns": [
@@ -13,6 +17,34 @@ $('#table_id').DataTable( {
         { data : "additionalInfo.messageTimestamp" },
     ],
     rowId: '_id.$oid',
-    
+    initComplete: function () {
+            // Apply the search
+            this.api()
+                .columns()
+                .every(function () {
+                    var that = this;
+
+                    $('input', this.footer()).on('keyup change clear', function () {
+                        if (that.search() !== this.value) {
+                            that.search(this.value).draw();
+                        }
+                    });
+                });
+        },
+        
+        responsive: {
+            details: {
+                display: $.fn.dataTable.Responsive.display.modal( {
+                    header: function ( row ) {
+                        var data = row.data();
+                        console.log(data)
+                        return 'Details for '+data.msgContext.appId /*+' '+data[1];*/;
+                    }
+                } ),
+                renderer: $.fn.dataTable.Responsive.renderer.tableAll( {
+                    tableClass: 'table'
+                } )
+            }
+        }
 } );
 
